ee453ab9c522e08e764f732907463813a58a30db
Ignacio Santiago Husain <santiago.husain@gmail.com>
2018-06-22 16:35:08 -0300

    Refactorizo creación del Mazo con la fábrica de cartas genérica.

--- src/carta/FabricaCartas.java
+++ src/carta/FabricaCartas.java
@@ -6,6 +6,8 @@ import carta.magica.FabricaCartasMagicas;
 import carta.monstruo.FabricaCartasMonstruo;
 import carta.trampa.FabricaCartasTrampa;
 
+import java.util.ArrayList;
+
 public class FabricaCartas
 {
     private FabricaCartasCampo fabricaCartasCampo;
@@ -40,5 +42,25 @@ public class FabricaCartas
     public CartaTrampa crearCartaTrampa(String nombre)
     {
         return this.fabricaCartasTrampa.obtenerCarta(nombre);
+    }
+
+    public ArrayList<String> obtenerNombresCartasCampo(){
+        return this.fabricaCartasCampo.obtenerNombres();
+    }
+
+    public ArrayList<String> obtenerNombresCartasMagicas(){
+        return this.fabricaCartasMagicas.obtenerNombres();
+    }
+
+    public ArrayList<String> obtenerNombresCartasTrampa(){
+        return this.fabricaCartasTrampa.obtenerNombres();
+    }
+
+    public ArrayList<String> obtenerNombresCartasMonstruosNormales(){
+        return this.fabricaCartasMonstruo.obtenerNombresMonstruosNormales();
+    }
+
+    public ArrayList<String> obtenerNombresCartasMonstruosNoNormales(){
+        return this.fabricaCartasMonstruo.obtenerNombresMonstruosNoNormales();
     }
 }
--- src/carta/Mazo.java
+++ src/carta/Mazo.java
@@ -1,10 +1,6 @@
 package carta;
 
 import AlGoOh.Jugador;
-import carta.campo.FabricaCartasCampo;
-import carta.magica.FabricaCartasMagicas;
-import carta.monstruo.FabricaCartasMonstruo;
-import carta.trampa.FabricaCartasTrampa;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -17,46 +13,43 @@ public class Mazo
     {
         this.cartas = new Stack<Carta>();
 
-        FabricaCartasMonstruo fabricaCartasMonstruo = new FabricaCartasMonstruo(jugador,oponente);
-        ArrayList<String> nombresMonstruosNormales = fabricaCartasMonstruo.obtenerNombresMonstruosNormales();
-        ArrayList<String> nombresMonstruosNoNormales = fabricaCartasMonstruo.obtenerNombresMonstruosNoNormales();
+        FabricaCartas fabricaCartas = new FabricaCartas(jugador, oponente);
 
-        FabricaCartasMagicas fabricaCartasMagicas = new FabricaCartasMagicas(jugador,oponente);
-        ArrayList<String> nombresMagicas = fabricaCartasMagicas.obtenerNombres();
-
-        FabricaCartasCampo fabricaCartasCampo = new FabricaCartasCampo(jugador,oponente);
-        ArrayList<String> nombresCampo = fabricaCartasCampo.obtenerNombres();
-
-        FabricaCartasTrampa fabricaCartasTrampa = new FabricaCartasTrampa(jugador,oponente);
-        ArrayList<String> nombresTrampas = fabricaCartasTrampa.obtenerNombres();
-
+        ArrayList<String> nombresMonstruosNoNormales = fabricaCartas.obtenerNombresCartasMonstruosNoNormales();
         for (int i = 0; i < nombresMonstruosNoNormales.size() && cantidadCartas > 0; i++)
         {
-            cartas.push(fabricaCartasMonstruo.obtenerCarta(nombresMonstruosNoNormales.get(i)));
+            cartas.push(fabricaCartas.crearCartaMonstruo(nombresMonstruosNoNormales.get(i)));
             cantidadCartas--;
         }
+
+        ArrayList<String> nombresMagicas = fabricaCartas.obtenerNombresCartasMagicas();
         for (int i = 0; i < nombresMagicas.size() && cantidadCartas > 0; i++)
         {
-            cartas.push(fabricaCartasMagicas.obtenerCarta(nombresMagicas.get(i)));
+            cartas.push(fabricaCartas.crearCartaMagica(nombresMagicas.get(i)));
             cantidadCartas--;
         }
+
+        ArrayList<String> nombresCampo = fabricaCartas.obtenerNombresCartasCampo();
         for (int i = 0; i < nombresCampo.size() && cantidadCartas > 0; i++)
         {
-            cartas.push(fabricaCartasCampo.obtenerCarta(nombresCampo.get(i)));
+            cartas.push(fabricaCartas.crearCartaCampo(nombresCampo.get(i)));
             cantidadCartas--;
         }
+
+        ArrayList<String> nombresTrampas = fabricaCartas.obtenerNombresCartasTrampa();
         for (int i = 0; i < nombresTrampas.size() && cantidadCartas > 0; i++)
         {
-            cartas.push(fabricaCartasTrampa.obtenerCarta(nombresTrampas.get(i)));
+            cartas.push(fabricaCartas.crearCartaTrampa(nombresTrampas.get(i)));
             cantidadCartas--;
         }
 
         // Agrego todas las que se pueden repetir.
+        ArrayList<String> nombresMonstruosNormales = fabricaCartas.obtenerNombresCartasMonstruosNormales();
         for (int i = 0; cantidadCartas > 0; i++)
         {
             if (i == nombresMonstruosNormales.size())
                 i = 0;
-            cartas.push(fabricaCartasMonstruo.obtenerCarta(nombresMonstruosNormales.get(i)));
+            cartas.push(fabricaCartas.crearCartaMonstruo(nombresMonstruosNormales.get(i)));
             cantidadCartas--;
         }
 

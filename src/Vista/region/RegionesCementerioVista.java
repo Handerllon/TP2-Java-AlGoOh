caf07f321dc37c148bb24586bffcdad74f396d69
Handerllon <manuel121097@gmail.com>
2018-08-01 00:02:21 -0300

    Actualizacion cementerio

--- src/Vista/region/RegionesCementerioVista.java
+++ src/Vista/region/RegionesCementerioVista.java
@@ -2,7 +2,14 @@ package Vista.region;
 
 import Modelo.observadores.ObservadorDeModelo;
 import Vista.Vista;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 public class RegionesCementerioVista implements ObservadorDeModelo
 {
@@ -13,6 +20,8 @@ public class RegionesCementerioVista implements ObservadorDeModelo
     private Vista vista;
     private Button botonCementerioJugador;
     private Button botonCementerioOponente;
+	private StackPane stackJugador;
+	private StackPane stackOponente;
 
     // --------------------------------------------------------------------
     // Métodos de construcción e inicialización.
@@ -25,6 +34,9 @@ public class RegionesCementerioVista implements ObservadorDeModelo
 
         this.botonCementerioJugador = this.inicializarBoton();
         this.botonCementerioOponente = this.inicializarBoton();
+        
+        this.stackJugador = new StackPane();
+        this.stackOponente = new StackPane();
     }
 
     private Button inicializarBoton()
@@ -38,16 +50,16 @@ public class RegionesCementerioVista implements ObservadorDeModelo
         return boton;
     }
 
-    public Button getCementerioJugador()
+    public StackPane getCementerioJugador()
     {
 
-        return botonCementerioJugador;
+        return stackJugador;
     }
 
-    public Button getCementerioOponente()
+    public StackPane getCementerioOponente()
     {
 
-        return botonCementerioOponente;
+        return stackOponente;
     }
 
     // --------------------------------------------------------------------
@@ -75,6 +87,35 @@ public class RegionesCementerioVista implements ObservadorDeModelo
     public void ingresoCartaARegion()
     {
         //TODO: cuando hay al menos 1 carta en el cementerio, se debería mostrar 'rutaImagenReversoCarta'.
+    	
+    	Rectangle mazo1 = new Rectangle((this.vista.getResolucionHorizontal() * porcentajeDeAnchoDeLaCarta), 
+    			(this.vista.getResolucionVertical() * porcentajeDeAltoDeLaCarta));
+    	Rectangle mazo2 = new Rectangle((this.vista.getResolucionHorizontal() * porcentajeDeAnchoDeLaCarta), 
+    			(this.vista.getResolucionVertical() * porcentajeDeAltoDeLaCarta));
+    	
+    	Image img1 = new Image(rutaImagenReversoCarta);
+    	mazo1.setFill(new ImagePattern(img1));
+    	Image img2 = new Image(rutaImagenReversoCarta);
+    	mazo2.setFill(new ImagePattern(img2));
+    	
+    	Label label1 = new Label();
+    	Label label2 = new Label();
+    	
+    	label1.setAlignment(Pos.CENTER);
+    	label2.setAlignment(Pos.CENTER);
+    	
+    	label1.setStyle("-fx-text-fill: white");
+    	label2.setStyle("-fx-text-fill: white");
+    	
+    	label1.setText(Integer.toString(this.vista.getModelo().getJugador().getCantidadDeCartasEnMano()));
+    	label2.setText(Integer.toString(this.vista.getModelo().getOponente().getCantidadDeCartasEnMano()));
+    	    	
+    	this.stackJugador.setAlignment(Pos.CENTER_RIGHT);
+    	this.stackOponente.setAlignment(Pos.CENTER_LEFT);
+    	
+    	this.stackJugador.getChildren().addAll(mazo1,label1);
+    	this.stackOponente.getChildren().addAll(mazo2,label2);
+    	
         this.vista.actualizarDibujo();
     }
 
